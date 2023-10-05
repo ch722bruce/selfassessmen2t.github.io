59d8989cd5ec25067ea8c6eb2f08cba997801e0d
username <useremail>
2023-10-05 12:00:19 -0700

    first submit

--- airbnb_listings2/js/main.js
+++ airbnb_listings2/js/main.js
@@ -1,3 +1,38 @@
+$(document).ready(function() {
+  $.ajax({
+      url: 'package-lock.json', 
+      type: 'GET',
+      dataType: 'json',
+      success: function(data) {
+          if (data && data.length) {
+              for (let i = 0; i < Math.min(50, data.length); i++) {
+                  let listing = data[i];
+                  
+                  let listingCard = `
+                  <div class="col-4">
+                      <div class="listing card">
+                          <img src="${listing.image}" class="card-img-top" alt="AirBNB Listing" />
+                          <div class="card-body">
+                              <h5 class="card-title">${listing.title}</h5>
+                              <p class="card-text">${listing.description}</p>
+                              <a href="#" class="btn btn-primary">Start to Book</a>
+                          </div>
+                      </div>
+                  </div>`;
+
+                  $('#listings').append(listingCard);
+              }
+          } else {
+              $('#listings').append('<div class="col-12"><p>No listings available.</p></div>');
+          }
+      },
+      error: function(error) {
+          console.error('Error fetching the listings', error);
+          $('#listings').append('<div class="col-12"><p>Error loading listings.</p></div>');
+      }
+  });
+});
+
 function MainModule(listingsID = "#listings") {
   const me = {};
 
@@ -60,3 +95,28 @@ const main = MainModule();
 
 
 main.loadData();
+
+$(document).ready(function() {
+  // Make an AJAX request to get the JSON data
+  $.ajax({
+      url: 'package-lock.json', // Replace with your JSON file's path
+      type: 'GET',
+      dataType: 'json',
+      success: function(data) {
+          // Check if data exists and it has at least 50 listings
+          if (data && data.length >= 50) {
+              for (let i = 0; i < 50; i++) {
+                  let listing = data[i];
+                  // Assuming each listing is an object with a title and description, modify as per your data structure
+                  $('#listings').append('<div class="listing"><h3>' + listing.title + '</h3><p>' + listing.description + '</p></div>');
+              }
+          } else {
+              $('#listings').append('<p>No listings available.</p>');
+          }
+      },
+      error: function(error) {
+          console.error('Error fetching the listings', error);
+          $('#listings').append('<p>Error loading listings.</p>');
+      }
+  });
+});
