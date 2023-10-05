0e385dd999f46a96b31519f1050b5cdd4049affe
username <useremail>
2023-10-05 12:57:30 -0700

    thrid

--- airbnb_listings2/js/main.js
+++ airbnb_listings2/js/main.js
@@ -1,38 +1,40 @@
-$(document).ready(function() {
-  $.ajax({
-      url: 'package-lock.json', 
-      type: 'GET',
-      dataType: 'json',
-      success: function(data) {
-          if (data && data.length) {
-              for (let i = 0; i < Math.min(50, data.length); i++) {
-                  let listing = data[i];
+// $(document).ready(function() {
+//   $.ajax({
+//       url: './airbnb_sf_listings_500.json', 
+//       type: 'GET',
+//       dataType: 'json',
+//       success: function(data) {
+//           if (data && data.length) {
+//               for (let i = 0; i < Math.min(50, data.length); i++) {
+//                   let listing = data[i];
                   
-                  let listingCard = `
-                  <div class="col-4">
-                      <div class="listing card">
-                          <img src="${listing.image}" class="card-img-top" alt="AirBNB Listing" />
-                          <div class="card-body">
-                              <h5 class="card-title">${listing.title}</h5>
-                              <p class="card-text">${listing.description}</p>
-                              <a href="#" class="btn btn-primary">Start to Book</a>
-                          </div>
-                      </div>
-                  </div>`;
+//                   let listingCard = `
+//                   <div class="col-4">
+//                       <div class="listing card">
+//                           <img src="${listing.picture_url}" class="card-img-top" alt="AirBNB Listing" />
+//                           <div class="card-body">
+//                               <h5 class="card-title">${listing.name}</h5>
+//                               <p class="card-text">${listing.description}</p>
+//                               <div class="card-atri">${listing.ost_location}</div>
+//                               <a href="#" class="btn btn-primary">Start to Book</a>
+//                           </div>
+//                       </div>
+//                   </div>`;
 
-                  $('#listings').append(listingCard);
-              }
-          } else {
-              $('#listings').append('<div class="col-12"><p>No listings available.</p></div>');
-          }
-      },
-      error: function(error) {
-          console.error('Error fetching the listings', error);
-          $('#listings').append('<div class="col-12"><p>Error loading listings.</p></div>');
-      }
-  });
-});
+//                   $('#listings').append(listingCard);
+//               }
+//           } else {
+//               $('#listings').append('<div class="col-12"><p>No listings available.</p></div>');
+//           }
+//       },
+//       error: function(error) {
+//           console.error('Error fetching the listings', error);
+//           $('#listings').append('<div class="col-12"><p>Error loading listings.</p></div>');
+//       }
+//   });
+// });
 
+// in class example
 function MainModule(listingsID = "#listings") {
   const me = {};
 
@@ -50,6 +52,7 @@ function MainModule(listingsID = "#listings") {
     <div class="card-body">
       <h2 class="card-title">${listing.name}</h2>
       <div>${listing.price}</div>
+      <div>${listing.host_location}</div>
       <p class="card-text">
         Some quick example text to build on the card title and make up
         the bulk of the card's content.
@@ -62,6 +65,27 @@ function MainModule(listingsID = "#listings") {
 
   `;
   }
+
+  function getListingCode1(listing) {
+    return `
+    <div class="col-4">
+        <div class="listing card">
+            <img src="${listing.picture_url}" class="card-img-top" alt="${listing.name}" />
+            <div class="card-body">
+                <h2 class="card-title">${listing.name}</h2>
+                <p class="card-text">${listing.description.split('<br />')[0]}...</p>
+                <div><strong>Price:</strong> ${listing.price}</div>
+                <div><strong>Location:</strong> ${listing.neighbourhood}</div>
+                <div><strong>Host:</strong> <a href="${listing.host_url}" target="_blank">${listing.host_name}</a></div>
+                <div><strong>Rating:</strong> ${listing.review_scores_rating}/5 from ${listing.number_of_reviews} reviews</div>
+                <a href="${listing.listing_url}" class="btn btn-primary" target="_blank">View on Airbnb</a>
+            </div>
+        </div>
+        <!-- /card -->
+    </div>
+    `;
+}
+
 
   function redraw(listings) {
     listingsElement.innerHTML = "";
@@ -96,27 +120,4 @@ const main = MainModule();
 
 main.loadData();
 
-$(document).ready(function() {
-  // Make an AJAX request to get the JSON data
-  $.ajax({
-      url: 'package-lock.json', // Replace with your JSON file's path
-      type: 'GET',
-      dataType: 'json',
-      success: function(data) {
-          // Check if data exists and it has at least 50 listings
-          if (data && data.length >= 50) {
-              for (let i = 0; i < 50; i++) {
-                  let listing = data[i];
-                  // Assuming each listing is an object with a title and description, modify as per your data structure
-                  $('#listings').append('<div class="listing"><h3>' + listing.title + '</h3><p>' + listing.description + '</p></div>');
-              }
-          } else {
-              $('#listings').append('<p>No listings available.</p>');
-          }
-      },
-      error: function(error) {
-          console.error('Error fetching the listings', error);
-          $('#listings').append('<p>Error loading listings.</p>');
-      }
-  });
-});
+
